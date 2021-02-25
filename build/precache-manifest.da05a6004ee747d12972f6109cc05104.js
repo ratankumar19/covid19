b34e45fa8aad5b817f340728e04fb4869d722c53
ratankumar19 <ratankumar086@gmail.com>
2021-02-26 02:09:23 +0530

    Init commit

---
+++ build/precache-manifest.da05a6004ee747d12972f6109cc05104.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb867771ac15fb03702d85631ef0f73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baf9f6a0fbd61e913fdc",
+    "url": "/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "e30750852f19ddf84178",
+    "url": "/static/css/main.fbbdfd8c.chunk.css"
+  },
+  {
+    "revision": "baf9f6a0fbd61e913fdc",
+    "url": "/static/js/2.86fbd634.chunk.js"
+  },
+  {
+    "revision": "6788bbbeb96ac5b9fb2d2231815fe398",
+    "url": "/static/js/2.86fbd634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e30750852f19ddf84178",
+    "url": "/static/js/main.0fbf8483.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
